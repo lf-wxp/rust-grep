962a6e1155b8b1b1b396574e11abd107c214ff03
franciscowu(吴小平) <franciscowu@tencent.com>
2020-06-30 17:58:19 +0800

    finish the grep

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use std::env;
+use std::{ env, process };
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args.get(1).unwrap();
-    let filename = &args.get(2).unwrap();
-    println!("search for {}", query);
-    println!("in file {}", filename);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
